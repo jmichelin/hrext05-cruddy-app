254054a02259f5e8203db72834153af473e698f1
JohnMichelin <jmichelinphone@gmail.com>
2018-10-26 16:07:43 -0700

    feat(localstorge): add button to clear local storage values

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 /*
-listen for click event (add)
-add text (with key) to localStorage
-update display with text
-
-
 listen for click event (edit)
 update text in local storage (with key)
 update display with new text value
 
 
-listen for click event (del)
-delete from local storage (with key)
-
  */
 
 $(document).ready(function(){
   console.log("before\n", window.localStorage);
+
+  // add event listener
   $(".add-text-btn").on("click", function(){
-    // add text (with key) to localStorage
+    $(".show-text").empty();
     var curTextValue = $('#theKey').val(); // reading from <input>
     var curKeyValue = "theKey"; // change to dynamic key?
     localStorage.setItem(curKeyValue, curTextValue);
-    console.log("...after", window.localStorage);
-    // update display with text
+    $(".show-text").append(curTextValue);
   });
+
+  // remove item from app
+
+  // listen for click event (del)
+  $(".clear-cache-btn").on("click", function(){
+    // clear local storage
+    localStorage.clear();
+    $(".show-text").empty();
+  });
+
 });
